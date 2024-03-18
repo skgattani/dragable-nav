900ba41634489b3aab466f048a12c876753d0afc
skgattani <skgattani2000@yahoo.com>
2024-03-18 23:00:42 +0530

    Delete resizable-nav directory

--- resizable-nav/src/app/screens/profile/profile.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  template: `<h1>ProfileProfileProfileProfileProfileProfile ProfileProfile Profile Profile Profile Profile Profile 
-  <h1>ProfileProfileProfileProfileProfileProfile ProfileProfile Profile Profile Profile Profile Profile </h1>`,
-})
-export class ProfileComponent {}
